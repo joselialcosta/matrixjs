565a3b7e6255243ee6d92c48dcdb555f776ca743
joselialcosta <joselialcosta@gmail.com>
2017-10-19 21:07:19 -0200

    Implementa metodos para criar, colorir e limpar uma matriz E também os testes

--- src/matrix.js
+++ src/matrix.js
@@ -1,3 +1,22 @@
-module.exports.createMatrix = function() {
-  return [[0,0],[0,0],[0,0],[0,0],[0,0]]
+const createMatrix = function createMatrix(rows, columns) {
+	matrix = []
+
+	for(let rowIndex = 0; rowIndex < rows; rowIndex++ ) {
+		columnsList = []
+		for(let columnIndex = 0; columnIndex < columns; columnIndex++){
+			columnsList.push('O')
+		}
+		matrix.push(columnsList)
+	}
+  return matrix
+
 }
+module.exports.createMatrix = createMatrix
+module.exports.colorizePixel = function(matrix, coordX, coordY, color) {
+	return matrix[coordX][coordY] = color
+}
+
+module.exports.clean = function(matrix) {
+	return createMatrix(matrix.length, matrix[0].length) 
+}
+
--- test/matrix.spec.js
+++ test/matrix.spec.js
@@ -3,7 +3,7 @@ var assert = require('assert')
 
 
 describe('Matrix', function() {
-  it('should return an empty matrix with the given size', function() {
+  it('should return a matrix with the given size', function() {
     const myMatrix = matrix.createMatrix(5, 2)
 
     assert.equal(myMatrix.length, 5)
@@ -13,5 +13,26 @@ describe('Matrix', function() {
     assert.equal(myMatrix[2].length, 2)
     assert.equal(myMatrix[3].length, 2)
     assert.equal(myMatrix[4].length, 2)
+  })
+
+  it('should return an empty matrix', function() {
+  	const myMatrix = matrix.createMatrix(0,0)
+  	assert.equal(myMatrix.length,0)
+  })
+
+  it('should colorize the matrix given coords', function() {
+    const myMatrix = matrix.createMatrix(5, 2)
+    const newColorMatrix = matrix.colorizePixel(myMatrix, 0, 0, 'C')
+
+    assert.equal(newColorMatrix[0][0], 'C')
+
+  })
+
+  it('should clean the matrix', function() {
+  	const myMatrix = matrix.createMatrix(5, 2)
+    matrix.colorizePixel(myMatrix, 0, 0, 'C')
+    const newMatrix = matrix.clean(myMatrix)
+
+    assert.equal(newMatrix[0][0], 'O')
   })
 })
